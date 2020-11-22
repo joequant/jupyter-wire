e6d4c0a4b81f9f6bd9208a3986e566729d85545b
Joseph C Wang <joequant@gmail.com>
2020-11-22 22:55:53 +0800

    checkpoint

--- source/jupyter/wire/magic.d
+++ source/jupyter/wire/magic.d
@@ -11,15 +11,25 @@ struct MagicRunner {
    void run(string command) {
       import std.string;
       import std.regex;
+      import std.array : join;
       auto line_regex = regex(r"^%([a-zA-Z0-9]+)\s*");
       auto cell_regex = regex(r"^%%([a-zA-Z0-9]+)\s*");
+      string[] cell_string_array;
+      string[] cell_items;
+      string[] line_items;
       foreach (string i; splitLines(command)) {
-         auto m_line = matchFirst(i, line_regex);
-      	 if(m_line) {
+         if(matchFirst(i, line_regex)) {
+           line_items ~= i;
+	 } else if(matchFirst(i, cell_regex)) {
+	   cell_items ~= i;
+	 } else {
+	   cell_string_array ~= i;
 	 }
-         auto m_cell = matchFirst(i, cell_regex);
-	 if(m_cell) {
-	 }
+      }
+      auto cell_string = join(cell_string_array, "\n");
+      foreach (string i; cell_items) {
+      }
+      foreach (string i; line_items) {
       }
    }
    void register(Magic m) {
